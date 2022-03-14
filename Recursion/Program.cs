1144b6911bf2200e545d24fe9637ee837e016eb7
ragabmohamedragab <ragabmoh2ooo@gmail.com>
2022-03-14 04:03:32 +0200

    Add Tail Function

--- Recursion/Program.cs
+++ Recursion/Program.cs
@@ -15,6 +15,12 @@ namespace Recursion
 {
     class Program
     {
+        public static int Num = 1;
+
+        // Direct Recursion
+        #region
+        // Complexity Time :O(Power(2,N))
+        // Complexity Space :O(1)
         public static int Fabi(int N)
         {
             if (N < 2)
@@ -26,17 +32,62 @@ namespace Recursion
                 return Fabi(N - 1) + Fabi(N - 2);
             }
         }
+        // Direct Method
+        // Complexity Time :O(Power(1,N))
+        // Complexity Space :O(1)
         public static int Fact(int N)
         {
             if (N == 1) return 1;
             else
                 return N * Fact(N - 1);
         }
-
+        #endregion
+        // Void Main
+        #region
+        // Complexity Time :O(1)
+        // Complexity Space :O(1)
         static void Main(string[] args)
         {
             //Console.WriteLine(Fact(5));
-            Console.WriteLine(Fabi(5));
+            //Console.WriteLine(Fabi(5));
+            Odd();
         }
+        #endregion
+        // Indirect Recursion
+        #region
+        // Complexity Time :O(1)
+        // Complexity Space :O(1)
+        public static void Odd()
+        {
+            if (Num <= 10)
+            {
+                Console.WriteLine(Num+1);
+                Num++;
+                Even();
+            }
+            
+        }
+        // Complexity Time :O(1)
+        // Complexity Space :O(1)
+        public static void Even()
+        {
+            if (Num <= 10)
+            {
+                Console.WriteLine(Num -1);
+                Num++;
+                Odd();
+            }
+           
+        }
+        #endregion
+
+
+
+
+
+
+
+
+
     }
 }
