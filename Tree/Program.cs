946a2a7a71cc8e1a08dabd57e41f0ca0b671d332
ragabmohamedragab <ragabmoh2ooo@gmail.com>
2022-07-13 14:17:18 +0200

    End from Insert Tree .

--- Tree/Program.cs
+++ Tree/Program.cs
@@ -1,94 +1,119 @@
-﻿/*
-  Tree : It is used to resprsent Data As Hirecaly Format 
-     1- Root
-     2- Leafs
-     3- Node
-     4- Parent
-     5- Children
-     6- Length | Hight 
-     7- Size 
-     8- Internal Nodes
-     9- OutDegree
-    10- Indegree
-    12- Edges
-    13- brunches
-    11- Total Degree
-    
-
---- Types of Tree Binary Tree
-   1- Full Binary Tree : The Parent has zero or two children . 
-   2- Complete Binary Tree : All level is completed expect last level is started from left.
-   3- Perfect Binary Tree : All Level is Completed ... Parent has Two Children .
-   4- Balanced Binary Tree : Hight of tree is Log(n) ... | H(Left Side) - H(Right Side) | <=1
-   5- Degenerate Binary Tree [physilogical] : Every Node has one Nodes ..
-
- ** Implement tree
-  - Pre-order-traversal[root Left right ]
- 
- */
-using System;
-namespace Tree
-{ 
-    // Yup.. This is Structred of Tree
+﻿using System;
+namespace Tree {
+    // This is Structured of Node 
     class Node {
-        public long key;
+        public long Key;
         public Node Left;
         public Node Right;
-        public Node(int Key)
+        public Node(int data)
         {
-            this.key= Key;
-            this.Left = null;
-            this.Right = null;
+            Key = data;
+            Left = null;
+            Right = null;
         }
     }
-    // Yup .. This is Implementing of Binary Tree
-    class BTS {
-        public Node Root = null;
-        public Node Temp=null;
-        public void Add(int data)
+
+    class BST {
+        public Node Temp = null; // To Iterate at Tree
+        public Node Root = null; // To refer First Node
+        public Node Previous = null;  // holds Previous Node
+        public void Add(int key)
         {
-            Node node = new Node(data);
-            if (Root == null) // if tree is Empty
+            Node node = new Node(key);
+            node.Left = null;
+            node.Right = null;
+            if (Root == null)  // If Tree is Empty
             {
-                node.Left = null;
-                node.Right = null;
                 Root = node;
                 Temp = Root;
                 return;
             }
+
             Temp = Root;
-            if (Temp.key <= data)
+            while (Temp != null)
             {
-                while (Temp.Right!=null)
+                Previous = Temp;
+                if (Temp.Key < key)
                 {
-                    Temp= Temp.Right;
+                    Temp = Temp.Right;
                 }
-                node.Left = null;
-                node.Right = null;
-                Temp.Right = node;
+                else
+                {
+                    Temp = Temp.Left;
+                }
+            }
+            if (Previous.Key < key)
+            {
+
+                Previous.Right = node;
             }
             else
             {
-                while (Temp.Left != null)
+                Previous.Left = node;
+            }
+        }
+        public void AddHelper(int key)
+        {
+            Node node = new Node(key);
+            if (Root == null)  // If Tree is Empty
+            {
+                Root = node;
+                Temp = Root;
+                return;
+            }
+            else
+            {
+                Temp = Root;
+                Helper(Temp, key);
+            }
+
+        }
+
+        public void Helper(Node temp, int data)
+        {
+
+            if (temp == null)
+            {
+                Node node = new Node(data);
+                if (Previous.Key < data)
                 {
-                    Temp=Temp.Left;
+                    Previous.Right = node;
                 }
-                node.Left= null;
-                node.Right = null;
-                Temp.Left = node;
+
+                else
+                {
+                    Previous.Left = node;
+                }
+                return;
+            }
+            else
+            {
+                Previous = temp;
+                if (temp.Key < data)
+                {
+                    temp = temp.Right;
+                    Helper(temp, data);
+                }
+                else
+                {
+                    temp = temp.Left;
+                    Helper(temp, data);
+                }
             }
         }
     }
-    class Program
-    {
+    class Program {
+
         static void Main(string[] args)
         {
-          BTS bTS = new BTS();
-            bTS.Add(1);
-            bTS.Add(2);
-            bTS.Add(3);
-            bTS.Add(-1);
-            bTS.Add(0);
+            BST bST = new BST();
+            for (int i = 0; i < 4; i++)
+            {
+                bST.AddHelper(i);
+            }
         }
     }
 }
+
+
+
