d43aa91ce80dabc67b2da6fe12a3e8037c149ff3
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2022-08-14 19:24:11 +0200

    Check if two binary trees are identical or not

--- BFS/Program.cs
+++ BFS/Program.cs
@@ -4,23 +4,29 @@ using System.Collections;
 using System.Collections.Generic;
 
 namespace BFS {
+
     public class Node {
         public long key;
         public Node Left;
         public Node Right;
+        public static long Count = 0;
         public Node(long key)
         {
             this.key = key;
             Left = null;
             Right = null;
+            Count++;
         }
     }
     public class Tree {
+        #region Properties
         Node Root = null;
         Node Temp = null;
         Node Current = null;
         Queue<Node> MyQueue = new Queue<Node>();
+       public ArrayList arr1 = new ArrayList();
         Node Previous = null;
+        #endregion
         #region Insert
         public void AddNode(long key)
         {
@@ -193,6 +199,7 @@ namespace BFS {
 
 
         #endregion
+
         #region Deletion
        public void Deletion(long key)
         {
@@ -299,8 +306,132 @@ namespace BFS {
             #endregion
 
             GC.Collect();
-
         }
+
+        #endregion
+
+        #region Check if two binary trees are identical or not
+
+        #region Tree1
+
+        #region Recursion
+        public void RecursiveHelper_Tree1()
+        {
+            if (!IsEmpty())
+            {
+                Temp = Root;
+                Recursive_Tree1(Temp);
+            }
+        }
+         void Recursive_Tree1(Node Temp)
+        {
+           
+            if(Temp == null)
+            {
+                return;
+            }
+            arr1.Add(Temp.key);
+            Recursive_Tree1(Temp.Left);
+            Recursive_Tree1(Temp.Right);
+        }
+        #endregion
+
+        #region Print
+        public void PrintTree1()
+        {
+            foreach (var item in arr1)
+            {
+                Console.Write(item+" ");
+            }
+        }
+        #endregion
+        #endregion
+
+        
+
+       
+
+        #endregion
+    }
+    public class Tree2 {
+        Node Root = null;
+        Node Temp = null;
+        Node Previous = null;
+       public ArrayList arr2 = new ArrayList();
+
+        #region Insert
+        public void AddNode(long key)
+        {
+            if (Root == null)
+            {
+                Node node = new Node(key);
+                Root = node;
+                Temp = Root;
+                return;
+            }
+            Temp = Root;
+            AddRecursion(Temp, key);
+        }
+        void AddRecursion(Node temp, long data)
+        {
+            if (temp == null)
+            {
+                Node node = new Node(data);
+                if (Previous.key < data)
+                {
+                    Previous.Right = node;
+                }
+                else
+                {
+                    Previous.Left = node;
+                }
+                return;
+            }
+            else if (temp.key < data)
+            {
+                Previous = temp;
+                AddRecursion(temp.Right, data);
+            }
+            else
+            {
+                Previous = temp;
+                AddRecursion(temp.Left, data);
+            }
+        }
+        #endregion
+        #region Tree2
+
+        #region Recursion
+        public void RecursiveHelper_Tree2()
+        {
+            if (Root!=null)
+            {
+                Temp = Root;
+                Recursive_Tree2(Temp);
+            }
+        }
+        void Recursive_Tree2(Node Temp)
+        {
+
+            if (Temp == null)
+            {
+                return;
+            }
+            arr2.Add(Temp.key);
+            Recursive_Tree2(Temp.Left);
+            Recursive_Tree2(Temp.Right);
+        }
+        #endregion
+
+        #region Print
+        public void PrintTree2()
+        {
+            foreach (var item in arr2)
+            {
+                Console.Write(item + " ");
+            }
+        }
+        #endregion
 
         #endregion
     }
@@ -313,7 +444,29 @@ namespace BFS {
             tree.AddNode(2);
             tree.AddNode(5);
             tree.AddNode(1);
-            Console.WriteLine("Traversal By using Breadth First :");
+            tree.RecursiveHelper_Tree1();
+            Console.Write("First Tree: ");
+            tree.PrintTree1();
+            Tree2 tree2 = new Tree2();
+            tree2.AddNode(3);
+            tree2.AddNode(4);
+            tree2.AddNode(2);
+            tree2.AddNode(5);
+            tree2.AddNode(1);
+            tree2.RecursiveHelper_Tree2();
+            Console.Write("\nSecond Tree: ");
+            tree2.PrintTree2();
+            Console.Write("\nResult:");
+           dynamic result= Compare(tree.arr1, tree.arr1);
+            if (result != null)
+            {
+                Console.Write("They are Equal");
+            }
+            else
+            {
+                Console.Write("They aren't Equal");
+            }
+            Console.WriteLine("\nTraversal By using Breadth First :");
             tree.BFS();
             Console.WriteLine("\nTraversal By using Depth First=>Pre Order :");
             tree.DFS_PreOrder();
@@ -329,7 +482,27 @@ namespace BFS {
             tree.DFS_INOrder();
 
         }
+        #region Compare Between Tree1 && Tree2
+        public static dynamic Compare(ArrayList arr1, ArrayList arr2)
+        {
+            bool IsEqual = false;
+            foreach (var item in arr1)
+            {
+                if (arr2.Contains(item))
+                {
+                    IsEqual = true;
+                    continue;
+                }else {
+                    return null;
+                }
+            }
+            if (IsEqual) { return true; } else {
+                return null;
+            }
+        }
+        #endregion
     }
+
 }
 
 
