60f8f14fd587ca1afba5abf24402d61dc95a76e7
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2022-11-12 19:45:42 +0200

    End From Description Of Selection And Bubble Sorting

--- Selection Sort/Program.cs
+++ Selection Sort/Program.cs
@@ -1,4 +1,64 @@
 ﻿// Date : 31-10-2022
+/*
+ * Time Complexity : O(N^2)
+ * Space Complexity : O(1)
+ * 
+ * Example :
+ * arr[] = {4, 1, 3, 9, 7}
+ *  #Path 1
+ * index :    0   1  2  3   4  
+ * value :    4   1  3  9  7
+ * 
+ *  min=4 1 
+ *  i=0
+ *  j=1 2 3 4
+ *  if(min>arr[j]  // 4>1 true - 1>3 false - 1>9 false 1>9 false  
+ *   { 
+ *     min=arr[j];
+ *    }
+ *    swap(4,1)
+ *    ------
+ *    #path2
+ *    index : 0 1 2 3 4
+ *    value  : 1 4 3 9 7
+ *    min=4 3
+ *    i=1
+ *    j=2 3 4
+ *    if(min>arr[j] 4>3 true - 3>9 false - 3>7 false
+ *    {
+ *      min=arr[j];
+ *    }
+ *    swap(4,3)
+ *    -----------
+ *    #path3
+ *    index:  0 1 2 3 4 
+ *    value:   1 3 4 9 7
+ *    min=4
+ *    i=2
+ *    j=3 9
+ *    if(min>arr[j]) 4>9 false -4>7  
+ *    {
+ *      min=arr[j];
+ *    }
+ *    swap(4,4)
+ *    --------
+ *    #path4
+ *    index : 0 1 2 3 4
+ *    value :  1 3 4 9 7 
+ *    min=9 7
+ *    i=3
+ *    j=4
+ *    if(min>arr[j]) 9>7 true   
+ *    {
+ *      min=arr[j];
+ *    }
+ *    swap(9,7)
+ *    ----
+ *    finally
+ *    
+ *    ------------------
+ *    Result :  1 3 4 7 9 
+ */
 namespace SelectionSort {
    public class Selection {
         // Attributes
