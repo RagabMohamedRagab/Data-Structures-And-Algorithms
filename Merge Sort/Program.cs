71cdda6b8b26b5a3a2d4607b2ae820e0b87e7611
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2022-12-01 00:52:10 +0200

    Explain Merge between Two Arrays.

---
+++ Merge Sort/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * - What Is Merge ?
+ * That Means Combine Between Two Arrays in the another Array become Sorting.
+ * - How is it ?
+ * while(i<sizeofA1&&j<sizeofA2){
+ *   if(A1[i]<A2[j]){
+ *     A3[k]=A[i];
+ *     i++;
+ *   }else{
+ *     A3[k]=A[j];
+ *     j++;
+ *   }
+ *   k++;
+ * }
+ * while(i<sizeofA1){
+ *   A3[k]=A1[i];
+ *   i++;
+ *   k++;
+ * }
+ * while(j<sizeofA2){
+ *  A3[k]=A2[j];
+ *  j++;
+ *  k++;
+ * }
+ * 
+ */
+using System;
+namespace Merge {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+}
