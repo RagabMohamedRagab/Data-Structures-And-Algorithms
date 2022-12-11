0cab5fcd9fcbcd99c3e460a95f903d067b185019
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2022-12-11 21:03:20 +0200

    Functionlity Of Quick Sorting .

---
+++ Quick Sort/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+
+namespace Quick_Sort {
+    public class Quick
+    {
+        void Swap(ref int a,ref int b)
+        {
+            int t = a;
+            a = b;
+            b=t;
+        }
+        public int Action(int[]A,int Bl,int Br)
+        {
+            int l = Bl;
+            int r = Br;
+            int Pivot = l;
+            while (true)
+            {
+                while (A[Pivot] < A[r])
+                {
+                    r--;
+                }
+                if(Pivot==r)
+                {
+                    break;
+                }
+                if (A[Pivot] > A[r]){
+                    Swap(ref A[Pivot],ref A[r]);
+                    Pivot = r;
+                }
+                while (A[Pivot] > A[l])
+                {
+                    l++;
+                }
+
+                if (Pivot == l)
+                {
+                    break;
+                }
+                if (A[Pivot] < A[l])
+                {
+                    Swap(ref A[Pivot],ref A[l]);
+                    Pivot = l;
+                }
+            } 
+            return Pivot;
+        }
+    }
+    public class Program {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
