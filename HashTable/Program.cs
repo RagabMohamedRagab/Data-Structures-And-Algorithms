898c7ed8da433b3442aaaa43eca8b7d316cf60b0
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2023-01-15 01:02:52 +0200

    Hash Table

---
+++ HashTable/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace HashTable {
+    public class Entry<T>
+    {
+        public int key;
+        public T value;
+        public Entry<T> Next;
+        public Entry(int key, T value)
+        {
+            this.key = key;
+            this.value = value;
+            this.Next = null;
+        }
+    }
+    public class Hash
+    {
+        public int size;
+        public int []Table; 
+        public Hash(int size)
+        {
+            this.size = size;
+            Table= new int[this.size];
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
