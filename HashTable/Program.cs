5d21ee3ea75c9bd0096f7ce96c87a6f7db9013d7
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2023-01-17 21:27:17 +0200

    End From HashTable Coding

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -1,32 +1,46 @@
-﻿using System;
+﻿// Efficent in Access Element 
+using System;
 namespace HashTable {
-    public class Entry<T>
-    {
-        public int key;
-        public T value;
-        public Entry<T> Next;
-        public Entry(int key, T value)
+    public class Hash {
+        public string Data { get; set; }
+        public Hash Next { get; set; }
+        public Hash()
         {
-            this.key = key;
-            this.value = value;
-            this.Next = null;
+            Next = null;
         }
     }
-    public class Hash
-    {
-        public int size;
-        public int []Table; 
-        public Hash(int size)
-        {
-            this.size = size;
-            Table= new int[this.size];
-        }
-    }
-    class Program
-    {
+    class Program {
         static void Main(string[] args)
         {
+            Dictionary<string, Hash> dic = new Dictionary<string, Hash>();
+            Console.Write("Number Of Data:");
+            int size = Int32.Parse(Console.ReadLine());
+            int i = 0;
+            while (i++ <= size)
+            {
+                Console.Write("PLZ Enter Key:");
+                string Key = Console.ReadLine().ToLower();
+                Console.Write("PLZ Enter Object:");
+                string content = Console.ReadLine();
+                Hash hash = new Hash() { Data = content, };
+                if (!dic.ContainsKey(Key))
+                {
 
+                    dic.Add(Key, hash);
+                }
+                else
+                {
+                    var data = dic[Key];
+                    while (data.Next != null)
+                    {
+                        data = data.Next;
+                    }
+                    data.Next = hash;
+                }
+            }
         }
     }
 }
+
+
+
