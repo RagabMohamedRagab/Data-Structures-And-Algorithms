773556deb20dc6643d29be3b70efedbaed834f8d
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2023-02-07 23:38:20 +0200

    Alter in Graph Coding

---
+++ Graph/Program.cs
@@ -0,0 +1,20 @@
+﻿/* 
+ * Graph : It Combines from Vertices and Edges.
+ * Representation Grap 
+ *      1- Matrix : Easy Code - Space Complexity O(V^2)
+ *      2- List  : Complex Code - Space Complexity O(V+E) - Dictionary
+ * Date   6-2-2023 10:04 PM
+*/
+namespace Graph {
+    class Vertice
+    {
+        public int  Key { get; set; }
+        public Vertice  Edge { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args) { 
+          Dictionary<string ,Vertice> GraphDb=new Dictionary<string ,Vertice>();
+        }
+    }
+}
