cbb5125d7fdbac1888bf95cf7280efd6e6912a7d
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2023-02-09 00:27:37 +0200

    Graph : Push Vertice in The Method .

--- Graph/Program.cs
+++ Graph/Program.cs
@@ -6,15 +6,57 @@
  * Date   6-2-2023 10:04 PM
 */
 namespace Graph {
-    class Vertice
+    public  class Vertice
     {
         public int  Key { get; set; }
         public Vertice  Edge { get; set; }
+        public Vertice()
+        {
+            Edge = null;
+        }
+    }
+    public class GraphList
+    {
+       static Dictionary<string, Vertice> GraphDb = new Dictionary<string, Vertice>();
+
+        public static void Push(string vertice, int data)
+        {
+            Vertice newVertice = new Vertice() { Key = data };
+            if (GraphDb.ContainsKey(vertice))
+            {
+                Vertice v = GraphDb[vertice];
+                Vertice temp = v;
+                while (temp.Edge != null)
+                {
+                    temp = temp.Edge;
+                }
+
+                temp.Edge = newVertice;
+            }
+            else
+            {
+                GraphDb.Add(vertice, newVertice);
+            }
+        }
     }
     class Program
     {
-        static void Main(string[] args) { 
-          Dictionary<string ,Vertice> GraphDb=new Dictionary<string ,Vertice>();
+        static void Main(string[] args) {
+            Console.Write("Number of Vertice:");
+            int size = Int32.Parse(Console.ReadLine());
+            int b = 0;
+            while(b< size)
+            {
+                Console.Write("Enter Vertice:");
+                string vertice=Console.ReadLine();
+                Console.Write("Enter Key");
+                int key=Int32.Parse(Console.ReadLine());
+                GraphList.Push(vertice, key);
+                b++;
+            }
+
         }
+       
+       
     }
 }
