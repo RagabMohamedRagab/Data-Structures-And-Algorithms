45b012b3cb1a11e3428c2ea4e95a4da959f51fdc
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2023-02-16 00:56:30 +0200

    End From Implement Push Method in Graph Matrix.

--- Graph/Program.cs
+++ Graph/Program.cs
@@ -8,24 +8,22 @@
 using System.Numerics;
 
 namespace Graph {
-    public  class Vertice
-    {
-        public string  Key { get; set; }
-        public Vertice  refer { get; set; }
+    public class Vertice {
+        public string Key { get; set; }
+        public Vertice refer { get; set; }
         public Vertice()
         {
             refer = null;
         }
     }
-    public class GraphList
-    {
-       static Dictionary<string, Vertice> GraphDb = new Dictionary<string, Vertice>();
+    public class GraphList {
+        static Dictionary<string, Vertice> GraphDb = new Dictionary<string, Vertice>();
         // Time Complexity
         //  Worst Case : O(N)
         // Average Case : O(N)
         // Best Case : O(1)
         // Space Complexity O(1)
-        public static void Push(string edeg,string vertice)
+        public static void Push(string edeg, string vertice)
         {
             Vertice newVertice = new Vertice() { Key = vertice };
             if (GraphDb.ContainsKey(edeg))
@@ -67,14 +65,14 @@ namespace Graph {
         // Space Complexity O(1)
         public static void GetAll()
         {
-            foreach(var key in GraphDb.Keys)
+            foreach (var key in GraphDb.Keys)
             {
-                Vertice temp= GraphDb[key];
+                Vertice temp = GraphDb[key];
                 Console.Write($"{key}: ");
                 while (temp != null)
                 {
                     Console.Write($"{temp.Key} ");
-                    temp=temp.refer;
+                    temp = temp.refer;
                 }
                 Console.WriteLine();
             }
@@ -84,14 +82,14 @@ namespace Graph {
         // Average Case : O(N)
         // Best Case : O(1)
         // Space Complexity O(1)
-        
+
         public static void Search(string key)
         {
             if (GraphDb.ContainsKey(key))
             {
                 Vertice temp = GraphDb[key];
                 Console.Write($"{key}:");
-                while(temp != null)
+                while (temp != null)
                 {
                     Console.Write($"{temp.Key} ");
                     temp = temp.refer;
@@ -102,60 +100,96 @@ namespace Graph {
                 Console.WriteLine("Not Found Key");
             }
         }
-         
+
     }
-    public class GraphMatrix
-    {
+    public class GraphMatrix {
         int size;
-       static int[,] Matrix = null;
+        static int[,] Matrix = null;
         public GraphMatrix(int N)
         {
             this.size = N;
             Matrix = new int[size, size];
         }
-      public static void GetAll()
+        public static void Push(int v, int e, int w)
+        {
+            Matrix[v, e] = w;
+            Matrix[e, v] = w;
+        }
+        public static void GetAll()
         {
             for (int i = 0; i < Matrix.GetLength(0); i++)
             {
-                for(int j = 0; j < Matrix.GetLength(1); j++)
+                for (int j = 0; j < Matrix.GetLength(1); j++)
                 {
-                    Console.Write(Matrix[i, j]+" ");
+                    Console.Write(Matrix[i, j] + " ");
                 }
                 Console.WriteLine();
             }
         }
     }
-    class Program
-    {
-        static void Main(string[] args) {
-            Console.Write("Number of Vertice:");
-            int size = Int32.Parse(Console.ReadLine());
-            int b = 0;
-            while (b < size)
-            {
-                Console.Write("Enter Edge:");
-                string edg = Console.ReadLine().ToUpper();
-                Console.Write("Enter Vertice:");
-                string vertice = Console.ReadLine().ToUpper();
+    class Program {
+        static void Main(string[] args)
+        {
+            //Console.Write("Number of Vertice:");
+            //int size = Int32.Parse(Console.ReadLine());
+            //int b = 0;
+            //while (b < size)
+            //{
+            //    Console.Write("Enter Edge:");
+            //    string edg = Console.ReadLine().ToUpper();
+            //    Console.Write("Enter Vertice:");
+            //    string vertice = Console.ReadLine().ToUpper();
 
-                GraphList.Push(edg, vertice);
-                b++;
-            }
-            Console.WriteLine();
-            Console.Write("Key you Would Like To Search: ");
-            string key = Console.ReadLine();
-            GraphList.Search(key);
-            Console.WriteLine();
-            GraphList.GetAll();
-            Console.Write("Enter Vertice you are removing it :");
-            string removeVertice = Console.ReadLine().ToUpper();
-            GraphList.Pop(removeVertice);
-            GraphList.GetAll();
+            //    GraphList.Push(edg, vertice);
+            //    b++;
+            //}
+            //Console.WriteLine();
+            //Console.Write("Key you Would Like To Search: ");
+            //string key = Console.ReadLine();
+            //GraphList.Search(key);
+            //Console.WriteLine();
+            //GraphList.GetAll();
+            //Console.Write("Enter Vertice you are removing it :");
+            //string removeVertice = Console.ReadLine().ToUpper();
+            //GraphList.Pop(removeVertice);
+            //GraphList.GetAll();
             // ----------------------------Represent by Matrix-----------------------
             Console.Write("Size of Matrix:");
             int matrix = Int32.Parse(Console.ReadLine());
             GraphMatrix graphMatrix = new GraphMatrix(matrix);
+            string[] Vertic = new string[matrix];
+            for (int i = 0; i < Vertic.Length; i++)
+            {
+                Vertic[i] = Console.ReadLine();
+            }
+            for (int a = 0; a < Vertic.Length; a++)
+            {
+                for (int b = 0; b < Vertic.Length; b++)
+                {
+                    Console.Write("Enter Edge:");
+                    string u = Console.ReadLine();
+                    Console.Write("Enter Vertice:");
+                    string v = Console.ReadLine();
+                    Console.Write("Weight :");
+                    int w = Int32.Parse(Console.ReadLine());
+                    int vindex = 0, eindex = 0;
+                    for (int j = 0; j < Vertic.Length; j++)
+                    {
+                        if (Vertic[j] == u)
+                        {
+                            eindex = j;
+                        }
+                        if (Vertic[j] == v)
+                        {
+                            vindex = j;
+                        }
+                    }
+                    GraphMatrix.Push(vindex, eindex, w);
+                }
+            }
+
             GraphMatrix.GetAll();
         }
     }
 }
+
