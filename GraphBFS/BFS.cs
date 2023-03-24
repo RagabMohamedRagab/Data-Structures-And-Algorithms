ef5dc4009e0fb4d9ee8999963c8f1fb90b17a03f
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2023-03-24 21:47:16 +0200

    Add Code of Breadth First Serach

--- GraphBFS/BFS.cs
+++
@@ -1,8 +0,0 @@
-﻿// Breadth First Search: It Takes Level By Level ...  
-// Time Complexity O(V+E)
-// Space Complexity O(V)
-// 
-namespace GraphBFS {
-    public  class BFS {
-    }
-}
---
+++ GraphBFS/Program.cs
@@ -0,0 +1,53 @@
+﻿// Breadth First Search
+// Time Complexity O(V+E)
+// Space Complexity O(V)
+namespace GraphBFS {
+    public class BFS
+    {
+        private int vertice;
+        private LinkedList<int>[] bfs;
+        /*
+         * {
+         *    new LinkedList<int>{1,2},
+         *    new LinkedList<int>{1,2},
+         *    new LinkedList<int>{1,2},
+         *    new LinkedList<int>{1,2}
+         * }
+         * 
+         * 
+         * 
+         */
+        public BFS(int V)
+        {
+            vertice= V;
+            bfs=new LinkedList<int>[vertice];
+            for(int i = 0; i < V; i++)
+            {
+                bfs[i]=new LinkedList<int>();
+            }
+        }
+
+
+        // Add Ede
+        public void Add(int v,int e)
+        {
+            bfs[v].AddLast(e);
+        }
+        public void Print()
+        {
+            foreach (var item in bfs)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+           BFS fS=new BFS(2);
+            fS.Print();
+         
+        }
+    }
+}
