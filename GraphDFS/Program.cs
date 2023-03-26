5dcc1017edc131cab275ad7a9c9fc79ab6092431
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2023-03-26 21:05:48 +0200

    End From Depth First Search by Iteration.

---
+++ GraphDFS/Program.cs
@@ -0,0 +1,65 @@
+﻿/* - Depth First Search..
+ *   using Stack to implemenation DFS 
+ *  1- Initial Stack by Adding Vertice.
+ *  2- Make it Visited
+ *  3- 
+ * 
+ * 
+ * 
+ */
+using System;
+namespace GraphDFS {
+    public class Graph {
+        private int V;
+        public LinkedList<int>[] dfs;
+        public bool[] Vistited;
+        public Graph(int v)
+        {
+            V = v;
+            Vistited=new bool[V];
+            dfs = new LinkedList<int>[v];
+            for (int i = 0; i < v; i++)
+            {
+                dfs[i] = new LinkedList<int>();
+            }
+        }
+        public void addEdge(int v, int e)
+        {
+            dfs[v].AddLast(e);
+        }
+        public void DFS(int v)
+        {
+             LinkedList<int> list = new LinkedList<int>();  
+            list.AddLast(v);
+            Vistited[v]= true;
+            while (list.Count > 0)
+            {
+                int i = list.Last.Value;
+                Console.Write($"{i} ");
+                list.RemoveLast();
+                foreach (var item in dfs[i])
+                {
+                    if (!Vistited[item])
+                    {
+                        list.AddLast(item);
+                        Vistited[item] = true;
+                    }
+                }
+            }
+        }
+    }
+
+    public class Program {
+        private static void Main(string[] args)
+        {
+            Graph g = new Graph(4);
+            g.addEdge(0, 1);
+            g.addEdge(0, 2);
+            g.addEdge(1, 2);
+            g.addEdge(2, 3);
+
+
+            g.DFS(0);
+        }
+    }
+}
