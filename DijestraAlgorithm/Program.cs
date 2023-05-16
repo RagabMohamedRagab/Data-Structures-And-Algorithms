8982e2dbb3cd94c6ff3d17ba12443d9b25857624
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2023-05-17 00:22:14 +0300

    End From Dijkestra's Algorithm Code.

--- DijestraAlgorithm/Program.cs
+++ DijestraAlgorithm/Program.cs
@@ -2,6 +2,8 @@
  -  Time Complexity O(V^2)
  - Aulixary Complexity O(V)
  */
+using System.Security.Cryptography.X509Certificates;
+
 namespace DijestraAlgorithm {
   public  class Graph
     {
@@ -21,13 +23,73 @@ namespace DijestraAlgorithm {
             dksta[v].Add((e, w));
             dksta[e].Add((v, w));
         }
+        public int[] Dijkestra(int init)
+        {
+            bool[] visited= new bool[v];
+            int[]dist= new int[v];
+            for(int a=0; a<v; a++)
+            {
+                dist[a] = int.MaxValue;
+            }
+            dist[init] = 0;
+            for(int k = 0; k < v - 1; k++)
+            {
+                int u = minimDistance(dist, visited);// Distance Between initial Vertice And neigbour Vertices;
+                visited[u]=true;
+                foreach ((int, int) edge in dksta[u])
+                {
+                    int v = edge.Item1;
+                    int w= edge.Item2;
+                    if (!visited[v] && dist[u] + w <= dist[v])
+                    {
+                        dist[v] = dist[u] + w;
+                    }
+                }
+            }
+            return dist;
+        }
+        public int minimDistance(int[] distance, bool[]vistited)
+        {
+            int minimValue = int.MaxValue;
+            int minimIndex = -1;
+            for(int b = 0; b < v; b++)
+            {
+                if (!vistited[b] && distance[b] <= minimValue)
+                {
+                    minimValue = distance[b];
+                    minimIndex = b;
+                }
+            }
+            return minimIndex;
+        }
     }
     class Program
     {
         private static void Main(string[]args)
         {
-          
-           
+            Graph graph = new Graph(9);
+            graph.AddEdge(0, 1, 4);
+            graph.AddEdge(0, 7, 8);
+            graph.AddEdge(1, 2, 8);
+            graph.AddEdge(1, 7, 11);
+            graph.AddEdge(2, 3, 7);
+            graph.AddEdge(2, 8, 2);
+            graph.AddEdge(2, 5, 4);
+            graph.AddEdge(3, 4, 9);
+            graph.AddEdge(3, 5, 14);
+            graph.AddEdge(4, 5, 10);
+            graph.AddEdge(5, 6, 2);
+            graph.AddEdge(6, 7, 1);
+            graph.AddEdge(6, 8, 6);
+            graph.AddEdge(7, 8, 7);
+            int[] dist = graph.Dijkestra(0);
+
+            Console.WriteLine("Vertex Distance from Source");
+            for (int i = 0; i < dist.Length; i++)
+            {
+                Console.WriteLine("{0}\t\t{1}", i, dist[i]);
+            }
+
         }
     }
 }
