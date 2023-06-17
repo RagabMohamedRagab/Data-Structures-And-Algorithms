95a5cdd350d36b4a661b23eded87c49420c6d1e5
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2023-06-18 00:46:47 +0300

    Start Coding Bellman Ford Greedy Algorithms.

--- BellmanFord/Program.cs
+++ BellmanFord/Program.cs
@@ -1,1 +1,28 @@
-﻿//   space complexity is O(V).
+﻿// Directed Graph
+using System;
+namespace BellmanFord {
+    public class Bellman {
+        public int verices;
+        public List<(int, int)>[] bellmanFord;
+        public Bellman(int v)
+        {
+             verices = v;
+            bellmanFord=new List<(int, int)>[v];
+            for(int i = 0; i < bellmanFord.Length; i++)
+            {
+                bellmanFord[i] = new List<(int, int)>();
+            }
+        }
+        public void AddEdges(int v,int E,int w)
+        {
+            bellmanFord[v].Add((E,w));
+        }
+    }
+    class Program {
+
+        private static void Main(string[] args)
+        {
+
+        }
+    }
+}
